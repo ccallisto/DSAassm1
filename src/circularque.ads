e9809b05c583a3a0dc94de9b0955d2ff5aa7f4f3
ccallisto <gavinsingleton23@gmail.com>
2024-01-20 14:09:10 -0600

    First Commit

---
+++ src/circularque.ads
@@ -0,0 +1,21 @@
+with Ada.text_IO; use Ada.Text_IO;
+
+generic
+    type message is private;
+    -- Add generic parameter to determine the size of the storage space.
+
+ package CircularQue is
+
+   procedure acceptMessage(msg: in message);
+
+   procedure retrieveMessage(msg: out message);
+
+   function circularQueEmpty return Boolean;
+
+   function circularQueFull return Boolean;
+
+   --Add method (function or procedure) for inserting at front of queue here and in body.
+   procedure Enqueue(msg: in message);
+
+
+ end CircularQue;
