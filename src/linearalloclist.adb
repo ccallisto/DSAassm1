54c382430fd7d7b4a335ba4cdfa8959faee75a2f
ccallisto <gavinsingleton23@gmail.com>
2024-01-30 12:44:18 -0600

    added binarysearch implementation

--- src/linearalloclist.adb
+++ src/linearalloclist.adb
@@ -11,7 +11,7 @@ package body LinearAllocList is
    box : array(slotindex) of message;
    max: Natural := capacity -1;
    
-   procedure insert(msg: in message) is 
+   procedure insert(msg: in Food_Pack) is 
    pos := last;
    while pos > 0 and then box(pos - 1) > msg loop
       box(pos) := box(pos - 1); -- Shift elements to the right
@@ -20,25 +20,48 @@ package body LinearAllocList is
 
    box(pos) := msg;
 
-         last := last + 1;
+   last := last + 1;
       else 
          put("ERROR - Message rejected - list is full!"); new_line(2);
     end if;
    end insert;
    
-   procedure remove(msg : out message)is
+   procedure retrieve(target :in Food_Pack; outitem : out Food_Pack)is
    begin
+      index := slotindex;
+      
+      if not isEmpty then
+      
+         index := BinarySearch(box, target)
+           
+         -- if binarysearch finds an index (not -1?) then remove that value
+         -- else send the index 0 and shift everything backward
+         
+      end if;
       --implement binarysearch to find particular index here
-   end remove;
+   end retrieve;
    
-   function BinarySearch(arr : in array of message; target : in message) return slotindex is
+   function BinarySearch(box : in array slotindex of Food_Pack; target : in Food_Pack) return slotindex is
     low : slotindex := 0;
     high : slotindex := last - 1;
     middle : slotindex;
+    
+     while low <= high loop
+         middle := (low+high)/2;
+      if box(middle)> target then
+            high := middle-1;
+            
+      else if box(middle) < target then
+            low := middle + 1;
+               
+      else
+         return middle;  
       
+      end if;           
+     end loop;
+    return slotindex'First; --not found 
       
-      
-    end BinarySearch
+    end BinarySearch;
    function isFull return Boolean is
    begin
       if (last = max)
@@ -51,7 +74,10 @@ package body LinearAllocList is
    
    function isEmpty return Boolean is
    begin
-   
+      if (first = 0)
+      return True;
+      else;
+      return False;
    end isEmpty;
    
 end LinearAllocList;
